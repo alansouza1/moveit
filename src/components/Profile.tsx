fbd60364551976d371264e455c771d6ddfc46171
alansouza1 <alansouza4001@gmail.com>
2022-11-28 10:41:31 -0300

    feat: Cria o componente Profile.tsx e o adiciona na Home

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/alansouza1.png" alt="Alan Souza" />
+      <div>
+        <strong>Alan Souza</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
