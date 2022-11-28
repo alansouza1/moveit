77d4c3257b766cf35843f21a817398eef3d1b19d
alansouza1 <alansouza4001@gmail.com>
2022-11-28 13:34:17 -0300

    feat: Adiciona icone levelup no componente Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,10 @@ export function Profile() {
       <img src="https://github.com/alansouza1.png" alt="Alan Souza" />
       <div>
         <strong>Alan Souza</strong>
-        <p>Level 1</p>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
       </div>
     </div>
   );
