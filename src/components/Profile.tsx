4b363c0ff3d4ec6b4d31e81208ee5d32ad142901
alansouza1 <alansouza4001@gmail.com>
2022-11-28 11:07:50 -0300

    feat: Cria um módulo css para o componente Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,8 @@
+import styles from '../styles/components/Profile.module.css';
+
 export function Profile() {
   return (
-    <div>
+    <div className={styles.profileContainer}>
       <img src="https://github.com/alansouza1.png" alt="Alan Souza" />
       <div>
         <strong>Alan Souza</strong>
